810456c41f45dc03dd421633b65e0ee112fa25b5
Simon Walker <github@stwalkerster.co.uk>
2016-07-11 22:16:59 +0100

    update assembly version

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Reflection;
 //      Revision      }
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.0.*")]
+[assembly: AssemblyVersion("0.1.*")]
