126ae844a3263d32b241c2c5a9b6f973ed74b0f4
Simon Walker <github@stwalkerster.co.uk>
2016-07-14 23:33:07 +0100

    upd version number

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("0.1.0.*")]
-[assembly: AssemblyInformationalVersion("0.1.0-alpha")]
+[assembly: AssemblyInformationalVersion("0.1.1-alpha")]
