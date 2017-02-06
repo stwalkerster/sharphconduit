f81aa2b2663f6a6315fb703bba9c33a24a47b8c0
Simon Walker <github@stwalkerster.co.uk>
2017-02-06 20:28:09 +0000

    Bump version number

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Reflection;
 //      Revision      }
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.1.0.*")]
-[assembly: AssemblyInformationalVersion("0.1.2-alpha")]
+[assembly: AssemblyVersion("0.1.3.*")]
+[assembly: AssemblyInformationalVersion("0.1.3-alpha")]
